61396d70caaf8747e8f9c7b02b618dc9b0b3332c
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2022-09-30 03:22:08 -0300

    feat :sparkles: finished client section

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { clients } from "../constants"
+import styles from "../styles"
 
-function Clients() {
-  return (
-    <div>Clients</div>
-  )
-}
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-h-[192px]`}>
+          <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain hover:brightness-200" />
+        </div>
+      ))}
+    </div>
+  </section>
+)
+
 
 export default Clients
